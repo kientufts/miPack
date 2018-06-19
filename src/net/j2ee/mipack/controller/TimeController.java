4fc8b0852526ef0a3a3308114d7059c6de1abb67
Kientufts <letrungkien1991@gmail.com>
2018-06-19 03:30:26 -0700

    system clock show

--- src/net/j2ee/mipack/controller/TimeController.java
+++ src/net/j2ee/mipack/controller/TimeController.java
@@ -1,4 +1,41 @@
 package net.j2ee.mipack.controller;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@ManagedBean
+@SessionScoped
 public class TimeController {
+    private String hour;
+    private String day;
+
+    public void showHour(){
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormatHour = new SimpleDateFormat("hh:mm:ss a");
+        hour = simpleDateFormatHour.format(date);
+    }
+
+    public void showDay(){
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormatDay = new SimpleDateFormat("EEEE - MMMMM dd");
+        day = simpleDateFormatDay.format(date);
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
 }
